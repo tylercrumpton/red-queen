d3a43b4ff45e7187cb3aaca7e9b486b4c41f370b
Tyler Crumpton <tyler.crumpton@gmail.com>
2015-04-08 00:15:51 -0500

    Added create_key route and updated others.

--- rqmanage/api.py
+++ rqmanage/api.py
@@ -3,9 +3,35 @@ from flask import Flask, make_response, jsonify, request
 RQ_VERSION = "v0.1.0"
 app = Flask(__name__)
 
+
+def get_owner(key):
+    return "tylercrumpton"
+
 @app.route("/api/v1.0/version", methods=["GET"])
 def get_api_version():
     return jsonify({'version': RQ_VERSION})
+
+@app.route('/api/v1.0/keys', methods=['POST'])
+def create_key():
+    if not request.json:
+        return make_response(jsonify({'error': 'No request body given'}), 400)
+    if 'owner_name' not in request.json:
+        return make_response(jsonify({'error': 'No owner_name given'}), 400)
+    if 'owner_email' not in request.json:
+        return make_response(jsonify({'error': 'No owner_email given'}), 400)
+
+    # TODO: Generate real id number
+    # TODO: Add key to DB
+    # TODO: Generate real key
+
+    key = {
+        'id': 1,
+        'owner_name': request.json['owner_name'],
+        'owner_email': request.json['owner_email'],
+        'key': '1234',
+    }
+
+    return jsonify({'key': key}), 201
 
 @app.route('/api/v1.0/endpoints', methods=['POST'])
 def create_endpoint():
@@ -13,21 +39,17 @@ def create_endpoint():
         return make_response(jsonify({'error': 'No request body given'}), 400)
     if 'endpoint_name' not in request.json:
         return make_response(jsonify({'error': 'No endpoint_name given'}), 400)
-    if 'owner_name' not in request.json:
-        return make_response(jsonify({'error': 'No owner_name given'}), 400)
-    if 'owner_email' not in request.json:
-        return make_response(jsonify({'error': 'No owner_email given'}), 400)
+    if 'key' not in request.json:
+        return make_response(jsonify({'error': 'No key given'}), 400)
 
     # TODO: Generate real id number
     # TODO: Add endpoint to DB
     # TODO: Create RabbitMQ queue for endpoint
-    # TODO: Generate endpoint management key
 
     endpoint = {
         'id': 1,
         'endpoint_name': request.json['endpoint_name'],
-        'owner_name': request.json['owner_name'],
-        'owner_email': request.json['owner_email'],
+        'owner_name': get_owner(request.json['key']),
         'description': request.json.get('description', ''),
     }
 
@@ -52,11 +74,33 @@ def send_message():
     message = {
         'id': 1,
         'target_endpoint': request.json['target_endpoint'],
-        'key': request.json['key'],
+        'sender': get_owner(request.json['key']),
         'data': request.json['data'],
     }
 
     return jsonify({'message': message}), 201
+
+@app.route('/api/v1.0/permissions/requests', methods=['POST'])
+def request_permission():
+    if not request.json:
+        return make_response(jsonify({'error': 'No request body given'}), 400)
+    if 'target_endpoint' not in request.json:
+        return make_response(jsonify({'error': 'No target_endpoint given'}), 400)
+    if 'key' not in request.json:
+        return make_response(jsonify({'error': 'No key given'}), 400)
+
+    # TODO: Generate real id number
+    # TODO: Add request to DB
+    # TODO: Send email to endpoint owner
+
+    perm_request = {
+        'id': 1,
+        'target_endpoint': request.json['target_endpoint'],
+        'requester': get_owner(request.json['key']),
+        'message': request.json.get('data', ''),
+    }
+
+    return jsonify({'request': perm_request}), 201
 
 @app.errorhandler(404)
 def not_found(error):
