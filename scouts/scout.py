963030b83a0917b5444dda10177d4acb2d358bbe
Tyler Crumpton <tyler.crumpton@gmail.com>
2015-04-14 23:52:58 -0500

    Added scout class.

---
+++ scouts/scout.py
@@ -0,0 +1,8 @@
+class RedQueenScout(object):
+    def __init__(self, rq_ip, rq_port):
+        self.queen_ip = rq_ip
+        self.queen_port = rq_port
+    def send_message(self, endpoint, data):
+        pass
+    def add_listener(self, endpoint, callback):
+        pass
