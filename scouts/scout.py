6e71b0142c683bd4c0daf28515a57b6fe1567fb0
Tyler Crumpton <tyler.crumpton@gmail.com>
2015-04-15 23:44:52 -0500

    Added info docstring.

--- scouts/scout.py
+++ scouts/scout.py
@@ -1,3 +1,9 @@
+""" RedQueenScout is a class that a project can use to communication with Red Queen. It handles the communications
+and protocols, leaving the developer to simply implement handlers for incoming data or use it to send messages of its
+own.
+"""
+
+
 class RedQueenScout(object):
     def __init__(self, rq_ip, rq_port):
         self.queen_ip = rq_ip
