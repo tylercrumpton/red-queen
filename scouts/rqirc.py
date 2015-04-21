f5f01d26c42fceb58d8d26352eea8703271f5815
Tyler Crumpton <tyler.crumpton@gmail.com>
2015-04-21 00:54:52 -0500

    Added throwaway code for rq irc bot.

--- scouts/rqirc.py
+++ scouts/rqirc.py
@@ -1,5 +1,8 @@
 import irc.bot
 import irc.strings
+from flask import Flask, request, jsonify
+import requests
+import threading
 
 class RQBot(irc.bot.SingleServerIRCBot):
     def __init__(self, channel, nick, server, port=6667):
@@ -15,7 +18,7 @@ class RQBot(irc.bot.SingleServerIRCBot):
     def on_pubmsg(self, connection, event):
         if event.arguments[0][0] == "!":
             a = event.arguments[0].split(" ", 1)
-            command = a[0].lstrip("!")
+            command = a[0].lstrip("!").rstrip(' ')
             arguments = None
             if len(a) > 1:
                 arguments = a[1].strip()
@@ -23,8 +26,29 @@ class RQBot(irc.bot.SingleServerIRCBot):
         return
 
     def handle_command(self, event, command, arguments):
-        print command, ": ", arguments
+        print "{command}: {arguments}".format(command=command, arguments=arguments)
+
+
+def send_message(destination, data, msg_type="event"):
+    payload = {'type': msg_type,
+               'key': '261brqmt1p68jshm0key',
+               'destination': destination,
+               'data': data}
+    r = requests.post("http://0.0.0.0:8080/api/v1.0/messages", data=jsonify(payload), timeout=1)
+
+app = Flask(__name__)
+
+def run_api():
+    app.run(host='0.0.0.0', port=9090, debug=False)
+
+@app.route('/message', methods=['POST'])
+def receive_message():
+    print request.json
+    return "OK"
 
 if __name__ == "__main__":
-    bot = RQBot("##rqtest", "PyRqBot", "chat.freenode.net", port=6667)
-    bot.start()
+    bot = RQBot("#btctiptest", "PyRqBot", "chat.freenode.net", port=6667)
+    irc_thread = threading.Thread(target=bot.start)
+    api_thread = threading.Thread(target=run_api)
+    irc_thread.start()
+    api_thread.start()
